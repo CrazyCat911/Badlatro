d1531f97f5beb644520bd46564d831f055493884
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2026-01-21 18:58:19 +1000

    first commit

---
+++ localization/bad.lua
@@ -0,0 +1,81 @@
+return {
+    descriptions = {
+        Back = {
+            b_abandoned = {
+                name = "Lonely Deck",
+                text = {
+                    "Start run with",
+                    "no {C:attention}friends{}."
+                }
+            },
+            b_anaglyph = {
+                name = "Anaglyph Deck Anaglyph Deck",
+                text = {
+                    "After defeating each",
+                    "{C:attention}Boss Blind{}, gain a",
+                    "{C:attention,T:tag_double}#1#"
+                }
+            },
+            b_black = {
+                name = "Business Deck",
+                text = {
+                    "You get",
+                    "{C:attention}+#1#{} Joker slot",
+                    "by paying",
+                    "{C:blue}-#2#{} hand!",
+                    "                Terms and Conditions apply.                ",
+                }
+            },
+            b_blue = {
+                name = "Sky-Colored Deck",
+                text = {
+                    "{C:blue}+#1#{} hand",
+                    "every round.",
+                    "Why would you need",
+                    "to carry so",
+                    "much stuff?"
+                }
+            },
+            b_challenge = { name = "Difficult Deck" },
+            b_checkered = {
+                name = "Picnic Deck",
+                text = {
+                    "All you need for a picnic:",
+                    "{C:attention}26{C:spades} Spades{} and",
+                    "{C:attention}26{C:hearts} Hearts{},",
+                    "So you can bury all of that",
+                    "unrequited love!"
+                }
+            },
+            b_erratic = {
+                name = "Crazy Deck",
+                text = {
+                    "Crazy? I was crazy once.",
+                    "They locked me in a room.",
+                    "A rubber room.",
+                    "A rubber room of rats.",
+                    "And rats make me crazy.",
+                    "Crazy? I was crazy once.",
+                    "They locked me in a room.",
+                    "A rubber room.",
+                    "A rubber room of rats.",
+                    "And rats make me crazy.",
+                    "Crazy? I was crazy once.",
+                    "They locked me in a room.",
+                    "A rubber room.",
+                    "A rubber room of rats.",
+                    "And rats make me crazy...",
+                }
+            },
+            b_ghost = {
+                name = "Invisible Deck",
+                text = {
+                    "{C:spectral}Spectral{} cards may, or may not",
+                    "appear in the shop and you also get to",
+                    "start with a {C:spectral,T:c_hex}Hex{} card.",
+                    "I don't know why you would want that..."
+                },
+            }
+        }
+    }
+}
---
+++ main.lua
@@ -0,0 +1,7 @@
+-- Load Bad Language
+SMODS.Language({
+    key = 'bad',
+    label = 'Badlatro language'
+})
+
+SMODS.Atlas({ key = "centers", path = "Enhancers.png", px = 71, py = 95, raw_key = true })
